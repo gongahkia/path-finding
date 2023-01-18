81b278e420b64626dd8cba47767e40c7fa2a5b6a
gong <angryapplegravy@gmail.com>
2023-01-19 00:20:26 +0800

    edited some unimportant text

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 #(1) function to create a dictionary based on coordinate values 
-# *takes in a template dictionary of only non space values as arguments (format of eg. startnode = [10,10]) and returns the coordinate dictionary
-# types of spaces == startnode, middlenode, endnode, empty spaces
+#*takes in a template dictionary of only non space values as arguments (format of eg. startnode = [10,10]) and returns the coordinate dictionary
+#types of spaces == startnode, middlenode, endnode, empty spaces
 
 def create_dict (**tank_dictionary):
     visual = {}
@@ -21,7 +21,7 @@ def create_dict (**tank_dictionary):
     return visual
 
 #(2) function to print said dictionary based on coordinate values
-# *takes in the aforementioned output from create_dict() as arguments and prints out the display accordingly
+#*takes in the aforementioned output from create_dict() as arguments and prints out the display accordingly
 def printer (visual:dict):
     for itervar in range (1,59):
         print ("X", end="")
